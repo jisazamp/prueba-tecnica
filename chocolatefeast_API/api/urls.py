bbad1722fe80d74537f548a679ec534ef48175a4
Juan P. Isaza <jisazamp@gmail.com>
2022-06-18 19:16:23 -0500

    Added Product editing

--- chocolatefeast_API/api/urls.py
+++ chocolatefeast_API/api/urls.py
@@ -3,6 +3,7 @@ from .views import ClientView
 from .views import StoreView
 from .views import OrderView
 from .views import PromotionView
+from .views import ProductView
 
 urlpatterns = [
     path("clients/", ClientView.as_view(), name="clients_list"),
@@ -13,4 +14,6 @@ urlpatterns = [
     path("orders/<int:id>", OrderView.as_view(), name="orders_process"),
     path("promotions/", PromotionView.as_view(), name="promotions_list"),
     path("promotions/<int:id>", PromotionView.as_view(), name="promotions_process"),
+    path("products/", ProductView.as_view(), name="products_list"),
+    path("products/<int:id>", ProductView.as_view(), name="products_process"),
 ]
--- chocolatefeast_API/api/views.py
+++ chocolatefeast_API/api/views.py
@@ -6,6 +6,7 @@ from .models import Client
 from .models import Store
 from .models import Order
 from .models import Promotion
+from .models import Product
 import json
 
 # Create your views here.
@@ -179,4 +180,40 @@ class PromotionView(View):
             data = {"message": "Sucess"}
         else:
             data = {"message": "Promotion not found..."}
+        return JsonResponse(data)
+
+
+class ProductView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id=0):
+        if id > 0:
+            products = list(Product.objects.filter(id=id).values())
+            if len(products) > 0:
+                product = products[0]
+                data = {"message": "Success", "product": product}
+            else:
+                data = {"message": "product not found..."}
+            return JsonResponse(data)
+        else:
+            products = list(Product.objects.values())
+            if len(products) > 0:
+                data = {"message": "Success", "products": products}
+            else:
+                data = {"message": "product not found..."}
+            return JsonResponse(data)
+
+    def put(self, request, id):
+        jd = json.loads(request.body)
+        products = list(Product.objects.filter(id=id).values())
+        if len(products) > 0:
+            product = Product.objects.get(id=id)
+            product.name = jd["name"]
+            product.price = jd["price"]
+            product.save()
+            data = {"message": "Sucess"}
+        else:
+            data = {"message": "Product not found..."}
         return JsonResponse(data)
